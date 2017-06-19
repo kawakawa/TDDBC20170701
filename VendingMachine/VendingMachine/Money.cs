5d6bfeb689ac696c5b609df9f1c079fd01c635fa
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-06-19 17:56:24 +0900

    お金を示すクラスを用意

---
+++ VendingMachine/VendingMachine/Money.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public enum MoneyType
+    {
+        Bill,//札
+        Coin//コイン
+    }
+
+
+    public class Money
+    {
+        private readonly int _money;
+        private readonly MoneyType _moneyType;
+
+        public Money(int money,MoneyType moneyType)
+        {
+            _money = money;
+            _moneyType = moneyType;
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine/MoneyKind.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class MoneyKind
+    {
+        //10円玉
+        public Money Money10 => new Money(10,MoneyType.Coin);
+
+        //50円玉
+        public Money Money50 => new Money(50, MoneyType.Coin);
+
+        //100円玉
+        public Money Money100 => new Money(100, MoneyType.Coin);
+
+        //500円玉
+        public Money Money500 => new Money(500, MoneyType.Coin);
+
+        //1000円札
+        public Money Money1000 => new Money(500, MoneyType.Bill);
+
+    }
+
+}
