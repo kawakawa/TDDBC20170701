614f9028fa21bf85079770f03c420164193fcee4
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-07 11:39:29 +0900

    投入金額クラス追加

--- VendingMachine/VendingMachineTests/Step1Tests.cs
+++ VendingMachine/VendingMachineTests/Step1Tests.cs
@@ -7,8 +7,11 @@ namespace VendingMachineTests
     public class Step1Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void _10円を投入して投入金額合計が10円になる()
         {
+
+
+
         }
     }
 }
