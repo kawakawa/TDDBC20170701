25a1dc590af1dff806b0ca9c209e865ae29ffdb5
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-11 10:41:56 +0900

    投入金額Class　リファクタリング

--- VendingMachine/VendingMachine/投入金額.cs
+++ VendingMachine/VendingMachine/投入金額.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VendingMachine
 {
     public class 投入金額
     {
 
-        private static List<Money.Money> _money=new List<Money.Money>();
+        private static List<Money.Money> Money=new List<Money.Money>();
 
 
 
         public static int Get合計金額()
         {
-            return 投入金額._money.Sum(n=>n.Value);
+            return Money.Sum(n=>n.Value);
         }
 
 
         public static void Add投入金(Money.Money money)
         {
-            投入金額._money.Add(money);
+            Money.Add(money);
         }
 
 
         public static void 初期化()
         {
-            _money.Clear();
+            Money.Clear();
         }
     }
 }
