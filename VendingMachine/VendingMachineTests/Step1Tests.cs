f28cfb85e0d11c8d80bc8a628ef0ae172b295b0c
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-11 10:20:26 +0900

    テスト名修正

--- VendingMachine/VendingMachineTests/Step1Tests.cs
+++ VendingMachine/VendingMachineTests/Step1Tests.cs
@@ -9,14 +9,14 @@ namespace VendingMachineTests
     public class Step1Tests
     {
         [TestMethod]
-        public void _10円を投入して投入金額合計が10円になる()
+        public void _10円玉を投入して投入金額合計が10円になる()
         {
             投入口.投入(MoneyKind.Yen10);
             投入金額.Get合計金額().Is(10);
         }
 
         [TestMethod]
-        public void _50円を投入して投入金額が50円になる()
+        public void _50円玉を投入して投入金額が50円になる()
         {
             投入口.投入(MoneyKind.Yen50);
             投入金額.Get合計金額().Is(50);
