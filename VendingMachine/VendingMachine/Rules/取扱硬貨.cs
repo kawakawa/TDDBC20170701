9289f52eaa049f0a0ba7ac46693272bcb792f49b
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-13 14:30:47 +0900

    リファクタリング

--- VendingMachine/VendingMachine/Rules/取扱硬貨.cs
+++ VendingMachine/VendingMachine/Rules/取扱硬貨.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VendingMachine.Rules
+﻿namespace VendingMachine.Rules
 {
     public static class 取扱硬貨
     {
@@ -15,15 +9,11 @@ namespace VendingMachine.Rules
         /// <returns></returns>
         public static bool Is取扱対象硬貨(Money.Money money)
         {
-            return money == Money.MoneyKind.Yen10
-                   ||
-                   money == Money.MoneyKind.Yen50
-                   ||
-                   money == Money.MoneyKind.Yen100
-                   ||
-                   money == Money.MoneyKind.Yen500
-                   ||
-                   money == Money.MoneyKind.Yen1000;
+            return    money == Money.MoneyKind.Yen10
+                   || money == Money.MoneyKind.Yen50
+                   || money == Money.MoneyKind.Yen100
+                   || money == Money.MoneyKind.Yen500
+                   || money == Money.MoneyKind.Yen1000;
         }
     }
 }
