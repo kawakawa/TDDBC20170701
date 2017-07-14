81f7c000e3e9c81652242774ef9ba43a1f014988
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-14 14:38:28 +0900

    リファクタリング

--- VendingMachine/VendingMachineTests/Step2Tests.cs
+++ VendingMachine/VendingMachineTests/Step2Tests.cs
@@ -8,19 +8,34 @@ namespace VendingMachineTests
     [TestClass]
     public class Step2Tests
     {
+
+        private Itemラック _itemラック;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _itemラック = Itemラック.ItemラックFactory();
+            _itemラック.格納アイテム初期化();
+        }
+
+        public static Item.Item GetCoke()
+        {
+            return Item.Items.Drink.Factory("コーラ", 100);
+        }
+
+
+
         [TestMethod]
         public void コーラを1本在庫として格納できる()
         {
             //コーラ
-            var coke = Item.Items.Drink.Factory("コーラ", 100);
+            var coke = GetCoke();
 
             //ラックに格納
-            var itemラック = Itemラック.ItemラックFactory();
-            itemラック.格納アイテム初期化();
+            _itemラック.SetItem(coke);
 
-            itemラック.SetItem(coke);
-
-            var 格納アイテムリスト = itemラック.GetItemList();
+            var 格納アイテムリスト = _itemラック.GetItemList();
             格納アイテムリスト.Count().Is(1);
             格納アイテムリスト.First().Name.Is(coke.Name);
             格納アイテムリスト.First().Price.Is(coke.Price);
@@ -31,16 +46,12 @@ namespace VendingMachineTests
         public void コーラを5本在庫として格納できる()
         {
             //コーラ
-            var coke = Item.Items.Drink.Factory("コーラ", 100);
-
-            //ラックに5本格納
-            var itemラック = Itemラック.ItemラックFactory();
-            itemラック.格納アイテム初期化();
-
+            var coke = GetCoke();
+            
             Enumerable.Range(1, 5).ToList()
-                .ForEach(i => itemラック.SetItem(coke));
+                .ForEach(i => _itemラック.SetItem(coke));
 
-            var 格納アイテムリスト = itemラック.GetItemList();
+            var 格納アイテムリスト = _itemラック.GetItemList();
             格納アイテムリスト.Count().Is(5);
             格納アイテムリスト.All(n => n.Name == coke.Name).IsTrue();
             格納アイテムリスト.All(n => n.Price == coke.Price).IsTrue();
