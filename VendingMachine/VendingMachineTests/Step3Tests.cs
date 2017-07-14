d596e85ead7b33062cc7a8d8a21a8e66f5eb3093
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-14 18:12:58 +0900

    リファクタリング

--- VendingMachine/VendingMachineTests/Step3Tests.cs
+++ VendingMachine/VendingMachineTests/Step3Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Item.Items;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Money;
 using VendingMachine;
@@ -8,22 +9,48 @@ namespace VendingMachineTests
     [TestClass]
     public class Step3Tests
     {
+
+        private Itemラック _itemラック;
+        private 投入金額 _投入金額;
+        private 投入口 _投入口;
+
+        private Item.Item _coke;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _coke= MakeCokeDrink();
+
+            _itemラック = Itemラック.ItemラックFactory();
+            _itemラック.格納アイテム初期化();
+
+            _投入金額= 投入金額.投入金額Factory();
+            _投入金額.投入金額歴初期化();
+
+            _投入口= 投入口.投入口Factory(_投入金額);
+        }
+
+        
+        public static Item.Item MakeCokeDrink()
+        {
+            return Item.Items.Drink.Factory("コーラ", 100);
+        }
+
+
+
         [TestMethod]
         public void コーラの在庫がある状態で100円投入して購入可能状態となるか()
         {
-            var coke= Item.Items.Drink.Factory("コーラ", 100);
-            var itemラック = Itemラック.ItemラックFactory();
-            itemラック.格納アイテム初期化();
+
             //ラックにcoke格納
-            itemラック.SetItem(coke);
+            _itemラック.SetItem(_coke);
 
-
-            var factory = 投入金額.投入金額Factory();
-            factory.投入金額歴初期化();
-            var _投入口 = 投入口.投入口Factory(factory);
+            //お金投入
             _投入口.投入(MoneyKind.Yen100);
 
-            VendingMachine.Rules.アイテム購入.Is購入可(coke.Name).IsTrue();
+            VendingMachine.Rules.アイテム購入
+                                .Is購入可(_coke.Name)
+                                .IsTrue();
         }
 
 
@@ -31,19 +58,16 @@ namespace VendingMachineTests
         [TestMethod]
         public void コーラの在庫がある状態で10円投入して購入不可状態となるか()
         {
-            var coke = Item.Items.Drink.Factory("コーラ", 100);
-            var itemラック = Itemラック.ItemラックFactory();
-            itemラック.格納アイテム初期化();
             //ラックにcoke格納
-            itemラック.SetItem(coke);
+            _itemラック.SetItem(_coke);
 
-
-            var factory = 投入金額.投入金額Factory();
-            factory.投入金額歴初期化();
-            var _投入口 = 投入口.投入口Factory(factory);
+            //お金投入
             _投入口.投入(MoneyKind.Yen10);
 
-            VendingMachine.Rules.アイテム購入.Is購入可(coke.Name).IsFalse();
+            VendingMachine.Rules.アイテム購入
+                .Is購入可(_coke.Name)
+                .IsFalse();
+
         }
 
 
