0d36ed32e280734e9188ddad615fd631affe9e12
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-14 17:51:31 +0900

    購入対象名を指定するように変更

--- VendingMachine/VendingMachine/Rules/アイテム購入.cs
+++ VendingMachine/VendingMachine/Rules/アイテム購入.cs
@@ -8,9 +8,25 @@ namespace VendingMachine.Rules
 {
     public static class アイテム購入
     {
-        public static bool Is購入可()
+        public static bool Is購入可(string 対象ItemName)
         {
-            return true;
+            var itemラック = Itemラック.ItemラックFactory();
+
+            //在庫なし=>購入不可
+            if (itemラック.GetItemList().All(n => n.Name != 対象ItemName))
+                return false;
+
+
+            var 対象アイテム = itemラック.GetItemList()
+                                        .First(n=>n.Name== 対象ItemName);
+
+            var 投入額 = 投入金額.投入金額Factory();
+            var 合計投入金額 = 投入額.Get合計金額();
+
+            if (対象アイテム.Price <= 合計投入金額)
+                return true;
+
+            return false;
         }
     }
 }
--- VendingMachine/VendingMachineTests/Step3Tests.cs
+++ VendingMachine/VendingMachineTests/Step3Tests.cs
@@ -23,7 +23,29 @@ namespace VendingMachineTests
             var _投入口 = 投入口.投入口Factory(factory);
             _投入口.投入(MoneyKind.Yen100);
 
-            VendingMachine.Rules.アイテム購入.Is購入可().IsTrue();
+            VendingMachine.Rules.アイテム購入.Is購入可(coke.Name).IsTrue();
         }
+
+
+
+        [TestMethod]
+        public void コーラの在庫がある状態で10円投入して購入不可状態となるか()
+        {
+            var coke = Item.Items.Drink.Factory("コーラ", 100);
+            var itemラック = Itemラック.ItemラックFactory();
+            itemラック.格納アイテム初期化();
+            //ラックにcoke格納
+            itemラック.SetItem(coke);
+
+
+            var factory = 投入金額.投入金額Factory();
+            factory.投入金額歴初期化();
+            var _投入口 = 投入口.投入口Factory(factory);
+            _投入口.投入(MoneyKind.Yen10);
+
+            VendingMachine.Rules.アイテム購入.Is購入可(coke.Name).IsFalse();
+        }
+
+
     }
 }
