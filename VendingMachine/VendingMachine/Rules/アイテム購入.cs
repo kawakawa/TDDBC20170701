4f72d3ef1bc3eff8fcf48d6d71f81e5f340e6f40
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-14 17:07:03 +0900

    [コーラの在庫がある状態で100円投入して購入可能状態となるか] テスト追加

---
+++ VendingMachine/VendingMachine/Rules/アイテム購入.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Rules
+{
+    public static class アイテム購入
+    {
+        public static bool Is購入可()
+        {
+            return true;
+        }
+    }
+}
---
+++ VendingMachine/VendingMachineTests/Step3Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Money;
+using VendingMachine;
+
+namespace VendingMachineTests
+{
+    [TestClass]
+    public class Step3Tests
+    {
+        [TestMethod]
+        public void コーラの在庫がある状態で100円投入して購入可能状態となるか()
+        {
+            var coke= Item.Items.Drink.Factory("コーラ", 100);
+            var itemラック = Itemラック.ItemラックFactory();
+            itemラック.格納アイテム初期化();
+            //ラックにcoke格納
+            itemラック.SetItem(coke);
+
+
+            var factory = 投入金額.投入金額Factory();
+            factory.投入金額歴初期化();
+            var _投入口 = 投入口.投入口Factory(factory);
+            _投入口.投入(MoneyKind.Yen100);
+
+            VendingMachine.Rules.アイテム購入.Is購入可().IsTrue();
+        }
+    }
+}
