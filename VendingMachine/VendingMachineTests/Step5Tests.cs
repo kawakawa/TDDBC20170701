86743c4fd731a41de62597ec9b3a5dd7bcf6efb0
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-25 11:24:54 +0900

    step5を「流れるようなインターフェース」を使ってリファクタリング

--- VendingMachine/VendingMachineTests/Step5Tests.cs
+++ VendingMachine/VendingMachineTests/Step5Tests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Money;
 using VendingMachine;
@@ -9,11 +10,7 @@ namespace VendingMachineTests
     [TestClass]
     public class Step5Tests
     {
-        private アイテムRack _アイテムRack;
-        private 投入金 _投入金;
-        private 投入口 _投入口;
-
-        private 売上金 _売上金;
+        private 自販機操作 _自販機操作;
 
         private readonly Item.Item _coke = Util.MakeCokeDrink();
         private readonly Item.Item _redbull = Util.MakeRedBullDrink();
@@ -23,22 +20,12 @@ namespace VendingMachineTests
         [TestInitialize]
         public void TestInitialize()
         {
-            _アイテムRack = Util.アイテムRack準備();
-            
-            _投入金 = Util.投入金準備();
-            _売上金 = Util.売上金準備();
+            _自販機操作=new 自販機操作();
 
-            _投入口 = 投入口.Factory(_投入金);
-
-
-            //Cokeを5本ラック格納
-            Util.アイテムRackにセット(_coke,5);
-
-            //レッドブルを5本ラック格納
-            Util.アイテムRackにセット(_redbull, 5);
-
-            //水を5本ラック格納
-            Util.アイテムRackにセット(_water, 5);
+            _自販機操作
+                .アイテムをラックに格納(対象アイテム: _coke,    個数: 5)
+                .アイテムをラックに格納(対象アイテム: _redbull, 個数: 5)
+                .アイテムをラックに格納(対象アイテム: _water,   個数: 5);
         }
 
 
@@ -46,231 +33,322 @@ namespace VendingMachineTests
         [TestMethod]
         public void _100円投入してコーラを1本購入した場合_お釣り0円とコーラ在庫4本となるか()
         {
-            _投入口.投入(MoneyKind.Yen100);
-
-            操作パネル.購入(_coke.Name);
-
-            Util.受取口からアイテム取出し()
-                .Name.Is(_coke.Name);
-            
-            Util.釣銭取出し()
-                .Get合計金額().Is(0);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name==_coke.Name).Is(4);
-
-
-            _売上金.GetTotal売上金額().Is(100);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen100)
+                .購入(アイテム名:_coke.Name)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_coke.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(0);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n=>n.Name==_coke.Name).Is(4);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(100);
+                    }
+                );
         }
 
 
         [TestMethod]
         public void _500円投入してコーラを1本購入した場合_お釣り400円とコーラ在庫4本となるか()
         {
-            _投入口.投入(MoneyKind.Yen500);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen500)
+                .購入(アイテム名: _coke.Name)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_coke.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(400);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _coke.Name).Is(4);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(100);
+                    }
+                );
             
-            操作パネル.購入(_coke.Name);
-
-            Util.受取口からアイテム取出し()
-                .Name.Is(_coke.Name);
-            
-            Util.釣銭取出し()
-                .Get合計金額().Is(400);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name == _coke.Name).Is(4);
-
-
-            _売上金.GetTotal売上金額().Is(100);
         }
 
 
         [TestMethod]
         public void _500円玉でコーラを5本を購入した場合_お釣り0円とコーラ在庫0本となるか()
         {
-            _投入口.投入(MoneyKind.Yen500);
 
-            Enumerable.Range(1, 5).ToList()
-                .ForEach(i =>
-                {
-
-                    操作パネル.購入(_coke.Name);
-
-                    Util.受取口からアイテム取出し()
-                            .Name.Is(_coke.Name);
-
-                    //お釣りを再度投入
-                    Util.釣銭取出し()
-                        .釣銭内容.ToList()
-                        .ForEach(money => _投入口.投入(money));
-                });
-
-            Util.釣銭取出し()
-                .Get合計金額().Is(0);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name == _coke.Name).Is(0);
-
-
-            _売上金.GetTotal売上金額().Is(500);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen500)
+                .繰り返し購入(アイテム名:_coke.Name,購入回数:5)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_coke.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(0);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _coke.Name).Is(0);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(500);
+                    }
+                )
+                ;
         }
 
 
         [TestMethod]
         public void _1000円札でコーラを5本を購入した場合_お釣り500円とコーラ在庫0本となるか()
         {
-            _投入口.投入(MoneyKind.Yen1000);
-
-            Enumerable.Range(1, 5).ToList()
-                .ForEach(i =>
-                {
-
-                    操作パネル.購入(_coke.Name);
-
-                    Util.受取口からアイテム取出し()
-                    .Name.Is(_coke.Name);
-
-                    //お釣りを再度投入
-                    Util.釣銭取出し()
-                        .釣銭内容.ToList()
-                        .ForEach(money => _投入口.投入(money));
-                });
-
-
-            操作パネル.払戻し();
-
-            Util.釣銭取出し()
-                .Get合計金額().Is(500);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name == _coke.Name).Is(0);
-
-            _売上金.GetTotal売上金額().Is(500);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen1000)
+                .繰り返し購入(アイテム名: _coke.Name, 購入回数: 5)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_coke.Name);
+                    }
+                )
+                .払い戻し()
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(500);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _coke.Name).Is(0);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(500);
+                    }
+                )
+                ;
+            
         }
 
         [TestMethod]
         public void _1000円札でレッドブルを5本を購入した場合_お釣り0円とレッドブル在庫0本となるか()
         {
-            _投入口.投入(MoneyKind.Yen1000);
 
-            Enumerable.Range(1, 5).ToList()
-                .ForEach(i =>
-                {
-
-                    操作パネル.購入(_redbull.Name);
-
-                    Util.受取口からアイテム取出し()
-                    .Name.Is(_redbull.Name);
-
-                    //お釣りを再度投入
-                    Util.釣銭取出し()
-                        .釣銭内容.ToList()
-                        .ForEach(money => _投入口.投入(money));
-                });
-
-
-            操作パネル.払戻し();
-
-            Util.釣銭取出し()
-                .Get合計金額().Is(0);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name == _redbull.Name).Is(0);
-
-            _売上金.GetTotal売上金額().Is(1000);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen1000)
+                .繰り返し購入(アイテム名: _redbull.Name, 購入回数: 5)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_redbull.Name);
+                    }
+                )
+                .払い戻し()
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(0);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _redbull.Name).Is(0);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(1000);
+                    }
+                )
+                ;
         }
 
 
         [TestMethod]
         public void _2000円札でレッドブルを5本を購入した場合_お釣り1000円とレッドブル在庫0本となるか()
         {
-            _投入口.投入(MoneyKind.Yen1000);
-            _投入口.投入(MoneyKind.Yen1000);
 
-            Enumerable.Range(1, 5).ToList()
-                .ForEach(i =>
-                {
-
-                    操作パネル.購入(_redbull.Name);
-
-                    Util.受取口からアイテム取出し()
-                    .Name.Is(_redbull.Name);
-
-                    //お釣りを再度投入
-                    Util.釣銭取出し()
-                        .釣銭内容.ToList()
-                        .ForEach(money => _投入口.投入(money));
-                });
-
-
-            操作パネル.払戻し();
-
-            Util.釣銭取出し()
-                .Get合計金額().Is(1000);
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-            格納アイテムリスト.Count(n => n.Name == _redbull.Name).Is(0);
-
-            _売上金.GetTotal売上金額().Is(1000);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen1000)
+                .お金投入(金額: MoneyKind.Yen1000)
+                .繰り返し購入(アイテム名: _redbull.Name, 購入回数: 5)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_redbull.Name);
+                    }
+                )
+                .払い戻し()
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(1000);
+                    }
+                )
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _redbull.Name).Is(0);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(1000);
+                    }
+                )
+                ;
         }
 
         [TestMethod]
         public void _1000円札でコーラ_レッドブル_水を1本ずつを購入した場合_お釣り600円と在庫がそれぞれ4本となるか()
         {
-            _投入口.投入(MoneyKind.Yen1000);
+            _自販機操作
+                .お金投入(金額: MoneyKind.Yen1000)
+                .購入(_coke.Name)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_coke.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(900);
+                        取り出した釣銭.釣銭内容.ToList()
+                            .ForEach(money => _自販機操作.お金投入(money));
+                    }
+                )
+
+                .購入(_redbull.Name)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_redbull.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(700);
+                        取り出した釣銭.釣銭内容.ToList()
+                            .ForEach(money => _自販機操作.お金投入(money));
+                    }
+                )
 
 
-            //コーラ購入
-            操作パネル.購入(_coke.Name);
+                .購入(_water.Name)
+                .取出し口から購入アイテム取出し
+                (
+                    取り出したアイテム =>
+                    {
+                        取り出したアイテム.IsNotNull();
+                        取り出したアイテム.Name.Is(_water.Name);
+                    }
+                )
+                .釣銭取出し
+                (
+                    取り出した釣銭 =>
+                    {
+                        取り出した釣銭.Get合計金額().Is(600);
+                    }
+                )
 
-            Util.受取口からアイテム取出し()
-                .Name.Is(_coke.Name);
-
-            //お釣りを再度投入
-            Util.釣銭取出し()
-                .釣銭内容.ToList()
-                .ForEach(money => _投入口.投入(money));
-
-
-            //レッドブル購入
-            操作パネル.購入(_redbull.Name);
-
-            Util.受取口からアイテム取出し()
-                .Name.Is(_redbull.Name);
-
-            //お釣りを再度投入
-            Util.釣銭取出し()
-                .釣銭内容.ToList()
-                .ForEach(money => _投入口.投入(money));
-
-
-            //水購入
-            操作パネル.購入(_water.Name);
-
-            Util.受取口からアイテム取出し()
-                .Name.Is(_water.Name);
-
-            //お釣りを再度投入
-            Util.釣銭取出し()
-                .釣銭内容.ToList()
-                .ForEach(money => _投入口.投入(money));
-            
-            
-            操作パネル.払戻し();
-
-            Util.釣銭取出し()
-                .Get合計金額().Is(600);
-
-
-            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
-
-            格納アイテムリスト.Count(n => n.Name == _coke.Name).Is(4);
-            格納アイテムリスト.Count(n => n.Name == _redbull.Name).Is(4);
-            格納アイテムリスト.Count(n => n.Name == _water.Name).Is(4);
-
-            _売上金.GetTotal売上金額().Is(400);
+                
+               
+                .格納アイテムリスト取得
+                (
+                    アイテムリスト =>
+                    {
+                        アイテムリスト.Count(n => n.Name == _coke.Name).Is(4);
+                        アイテムリスト.Count(n => n.Name == _redbull.Name).Is(4);
+                        アイテムリスト.Count(n => n.Name == _water.Name).Is(4);
+                    }
+                )
+                .売上金取得
+                (
+                    売上金 =>
+                    {
+                        売上金.Is(400);
+                    }
+                )
+                ;
         }
-
-
     }
 }
---
+++ VendingMachine/VendingMachineTests/自販機操作.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine;
+using VendingMachine.部位;
+
+namespace VendingMachineTests
+{
+    public class 自販機操作
+    {
+
+        private readonly アイテムRack _アイテムRack;
+        private readonly 投入金 _投入金;
+        private readonly 投入口 _投入口;
+        private readonly 売上金 _売上金;
+
+
+
+        public 自販機操作()
+        {
+            _アイテムRack = Util.アイテムRack準備();
+
+            _投入金 = Util.投入金準備();
+            _売上金 = Util.売上金準備();
+
+            _投入口 = 投入口.Factory(_投入金);
+        }
+
+
+        public 自販機操作 アイテムをラックに格納(Item.Item 対象アイテム, int 個数)
+        {
+            Util.アイテムRackにセット(対象アイテム,個数);
+            return this;
+        }
+
+        public 自販機操作 お金投入(Money.Money 金額)
+        {
+            _投入口.投入(金額);
+            return this;
+        }
+        
+        public 自販機操作 購入(string アイテム名)
+        {
+            操作パネル.購入(アイテム名);
+            return this;
+        }
+
+        public 自販機操作 繰り返し購入(string アイテム名, int 購入回数)
+        {
+            Enumerable.Range(1, 5).ToList()
+                .ForEach(i =>
+                {
+                    操作パネル.購入(アイテム名);
+
+                    //お釣りを再度投入
+                    Util.釣銭取出し()
+                        .釣銭内容.ToList()
+                        .ForEach(money => _投入口.投入(money));
+                });
+            return this;
+        }
+
+
+        public 自販機操作 取出し口から購入アイテム取出し(Action<Item.Item> action)
+        {
+            var 取り出したアイテム = Util.受取口からアイテム取出し();
+            action(取り出したアイテム);
+            return this;
+        }
+
+        public 自販機操作 払い戻し()
+        {
+            操作パネル.払戻し();
+            return this;
+        }
+
+        public 自販機操作 釣銭取出し(Action<釣銭> action)
+        {
+            var 取り出した釣銭= Util.釣銭取出し();
+            action(取り出した釣銭);
+            return this;
+        }
+
+
+        public 自販機操作 格納アイテムリスト取得(Action<IEnumerable<Item.Item>> action)
+        {
+            var 格納アイテムリスト = _アイテムRack.Get格納アイテムリスト();
+            action(格納アイテムリスト);
+            return this;
+        }
+
+        public 自販機操作 売上金取得(Action<int> action)
+        {
+            var 売上金=_売上金.GetTotal売上金額();
+            action(売上金);
+
+            return this;
+        }
+
+    }
+}
