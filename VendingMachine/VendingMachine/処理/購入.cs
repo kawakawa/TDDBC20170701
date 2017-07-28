9a0ee76a0ca3367eafb9caf03ef036763a272fdf
TakuyaKawabe <rapidarrow2000@gmail.com>
2017-07-28 11:44:00 +0900

    購入Classをリファクタリング

--- VendingMachine/VendingMachine/処理/購入.cs
+++ VendingMachine/VendingMachine/処理/購入.cs
@@ -6,23 +6,41 @@ namespace VendingMachine.処理
     {
         public static void 実行(string 購入対象アイテム)
         {
-
-            //アイテム在庫整理
-            var 購入アイテム=アイテムRack.Factory()
-                                      .アイテム取出し(購入対象アイテム);
+            var 購入アイテム = ラックから対象アイテム取出し(購入対象アイテム);
             
-            //購入アイテムを受取口にセット
-            アイテム受取口.Factory()
-                .Setアイテム(購入アイテム);
+            購入アイテムを受取口にセット(購入アイテム);
             
-            //売上計上
-            売上金.Factory()
-                .Add売上金(購入アイテム.Price);
+            売上金にアイテム金額を計上(購入アイテム);
 
-            //投入金額計算
+            投入金から購入金額の差分を払い戻し(購入アイテム);
+        }
+
+
+        private static void 投入金から購入金額の差分を払い戻し(Item.Item 購入アイテム)
+        {
             投入金.Factory()
                 .購入金額分減算(購入アイテム.Price)
                 .払い戻し();
+        }
+
+        private static void 売上金にアイテム金額を計上(Item.Item 購入アイテム)
+        {
+            売上金.Factory()
+                .Add売上金(購入アイテム.Price);
+        }
+
+        private static void 購入アイテムを受取口にセット(Item.Item 購入アイテム)
+        {
+            アイテム受取口.Factory()
+                .Setアイテム(購入アイテム);
+        }
+
+
+        private static Item.Item ラックから対象アイテム取出し(string 購入対象アイテム)
+        {
+            var 購入アイテム = アイテムRack.Factory()
+                .アイテム取出し(購入対象アイテム);
+            return 購入アイテム;
         }
     }
 }
